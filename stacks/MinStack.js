2e6793298f480a658da9802f2233bdc22edae5cd
Justin <90215009+DinerJ@users.noreply.github.com>
2026-01-19 00:51:07 -0500

    minStack better space approach

--- stacks/MinStack.js
+++ stacks/MinStack.js
@@ -35,3 +35,51 @@ class MinStack {
         return this.minArr[this.minArr.length - 1];
     }
 }
+
+// Different appraoch storing only the difference. Requires one less array. So if space is a concern, this is a better approach.
+class MinStack {
+    constructor() {
+        this.min = Infinity;
+        this.stack = [];
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        if (this.stack.length === 0) {
+            this.stack.push(0);
+            this.min = val;
+        } else {
+            this.stack.push(val - this.min);
+            if (val < this.min) this.min = val;
+        }
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        if (this.stack.length === 0) return;
+
+        const pop = this.stack.pop();
+
+        if (pop < 0) this.min -= pop;
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        const top = this.stack[this.stack.length - 1];
+        return top > 0 ? top + this.min : this.min;
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        return this.min;
+    }
+}
