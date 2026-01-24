197b08834d467bc3ffd804424ad6fc08292d8c48
Justin <90215009+DinerJ@users.noreply.github.com>
2026-01-23 23:10:51 -0500

    car fleet attempt

--- stacks/carFleet.js
+++ stacks/carFleet.js
@@ -21,3 +21,41 @@ carFleet(target, position, speed) {
 
     return stack.length;
 }
+
+// My solution
+// O(n log n) time, O(n) space
+class Solution {
+    /**
+     * @param {number} target
+     * @param {number[]} position
+     * @param {number[]} speed
+     * @return {number}
+     */
+    carFleet(target, position, speed) {
+        const cars = position.map((p, i) => {
+            return [p, speed[i]];
+        }).sort((a, b) => b[0] - a[0]);
+
+        const stack = [];
+        for (let car of cars) {
+            const [pos, s] = car;
+            const timeToTarget = (target - pos) / s;
+            if (stack.length > 0 && (timeToTarget <= stack[stack.length -1])) {
+                continue;
+            }
+            stack.push(timeToTarget);
+        }
+        return stack.length;
+    }
+ }
+
+ // timeToTarget = (target - position) / speed
+ // combine the arrays with position and speed into [position[i], speed[i]]
+ // sort the array in descending order. This is because the last position will be furthest along
+ // but will also lead the pack. If it is the slowest, everyone else is stuck behind it. 
+ // loop through the array with the positions first
+ // calculate timeToTarget for that and push onto stack. 
+ // on next item, if time to target <= than stack[stack.length -1], its stuck. It will join that fleet.
+ // if its more, add it to stack. 
+ // return stack.length 
+
