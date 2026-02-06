d1587f1d699bd87a244fa09cd946db81ff358e46
Justin <90215009+DinerJ@users.noreply.github.com>
2026-02-05 23:35:17 -0500

    MinWindowSubstring

--- sliding_windows/MinWindowSubstring.js
+++ sliding_windows/MinWindowSubstring.js
@@ -44,3 +44,50 @@ class Solution {
         return resLen === Infinity ? '' : s.slice(res[0], res[1] + 1);
     }
 }
+
+// My Solution
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {string}
+     */
+        minWindow(s, t) {
+            if (t === '' || t.length > s.length) {
+                return '';
+            }
+
+            let matches = 0;
+            let tCount = new Map();
+            let window = new Map();
+            let smallestLength = Infinity;
+            let res = [-1, -1]
+
+            for (let c of t) {
+                tCount.set(c, (tCount.get(c) || 0) + 1);
+            }
+
+            let L = 0;
+            for (let R = 0; R < s.length; R++) {
+                window.set(s[R], (window.get(s[R]) || 0) + 1);
+
+                if (window.get(s[R]) === tCount.get(s[R])) {
+                    matches++;
+                }
+
+                while (matches === tCount.size) {
+                    if ((R - L) + 1 < smallestLength) {
+                        smallestLength = (R - L)+1
+                        res = [L, R];
+                    }
+                    
+                    window.set(s[L], (window.get(s[L]) - 1));
+                    if (tCount.has(s[L]) && window.get(s[L]) < tCount.get(s[L])) {
+                        matches--;
+                    }
+                    L++;
+                }
+            }
+        return smallestLength === Infinity ? "" : s.slice(res[0], res[1] + 1)
+    }
+}
