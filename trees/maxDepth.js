96b932da6eabb50d1b013236052b093a68923364
Justin <90215009+DinerJ@users.noreply.github.com>
2026-02-28 23:55:37 -0500

    bfs solution to maxDepth

--- trees/maxDepth.js
+++ trees/maxDepth.js
@@ -53,3 +53,48 @@ class Solution {
         return max;
     }
 }
+
+// BFS Solution O(n) time complexity, O(n) space complexity.
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number}
+     */
+
+    maxDepth(root) {
+        if (root === null) {
+            return 0;
+        }
+
+        let level = 0;
+
+        const queue = [root];
+
+        while (queue.length > 0) {
+            const size = queue.length;
+            for (let i = 0; i < size; i++) {
+                const current = queue.shift();
+                if (current.left !== null) {
+                    queue.push(current.left);
+                }
+
+                if (current.right !== null) {
+                    queue.push(current.right)
+                };
+            }
+            level++;    
+        }
+        return level;
+    }
+}
