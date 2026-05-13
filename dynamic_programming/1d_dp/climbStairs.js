4d7e2c088e1bd4e0a46379628e5fa252a8062777
Justin <90215009+DinerJ@users.noreply.github.com>
2026-05-12 23:33:59 -0400

    climbStairs expanded

--- dynamic_programming/1d_dp/climbStairs.js
+++ dynamic_programming/1d_dp/climbStairs.js
@@ -21,3 +21,20 @@ climbStairs(n) {
     }
     return answer[1];
 }
+
+// Bototom Up With DP Array O(n) Time and O(n) Space
+climbStairs(n) {
+    if (n <= 2) {
+        return n;
+    }
+    const dp = new Array(n + 1).fill(-1);
+
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for (let i = 3; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i-2];
+    }
+
+    return dp[n];
+}
