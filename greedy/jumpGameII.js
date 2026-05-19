3781a85ea861a3d3a49a55a47acb6e8cc74b318b
Justin <90215009+DinerJ@users.noreply.github.com>
2026-05-19 00:05:50 -0400

    jump game II dynamic programming

--- greedy/jumpGameII.js
+++ greedy/jumpGameII.js
@@ -15,3 +15,31 @@ jump(nums) {
     }
     return minJumps;
 }
+
+// Dynamic Programming - O(n^2) time and O(n) Space
+jump(nums) {
+    const map = new Map();
+    const dfs = (i) => {
+        if (i === nums.length - 1) {
+            return 0;
+        }
+
+        if (i >= nums.length ) {
+            return Infinity;
+        }
+
+        if (map.has(i)) {
+            return map.get(i);
+        }
+
+        let min = Infinity;
+        for (let j = 1; j <= nums[i]; j++) {
+            min = Math.min(min, 1 + dfs(i + j));
+        }
+
+        map.set(i, min);
+        return map.get(i);
+    }
+
+    return dfs(0);
+}
